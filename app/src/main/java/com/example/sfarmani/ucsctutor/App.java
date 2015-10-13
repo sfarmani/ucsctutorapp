078367f858b6167bf2f254566e7f548e4955cf2a
Shayan Farmani <sfarmani@ucsc.edu>
2015-10-13 16:32:08 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/sfarmani/ucsctutor/App.java
@@ -0,0 +1,29 @@
+package com.example.sfarmani.ucsctutor;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+/**
+ * Created by Shayan Farmani on 10/13/2015.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        // defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
