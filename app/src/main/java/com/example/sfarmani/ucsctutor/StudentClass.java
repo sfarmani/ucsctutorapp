1f928567a1433d354cf0b5a6c1b5d5615d2de2eb
BRAD CARDELLO <bcardell@ucsc.edu>
2015-10-15 22:13:34 -0700

    Added a couple fields and methods to Student and Tutor classes.

--- app/src/main/java/com/example/sfarmani/ucsctutor/StudentClass.java
+++ app/src/main/java/com/example/sfarmani/ucsctutor/StudentClass.java
@@ -1,7 +1,16 @@
 package com.example.sfarmani.ucsctutor;
 
+import android.media.Image;
+
+import java.util.Map;
+
 /**
  * Created by Brad Cardello on 10/14/2015.
  */
 public class StudentClass {
+    String studentUserName;
+    String studentDesc;
+    Image studentProfPic;
+    // Map used to allow user to see how they rated a tutor
+    Map<String, Double> tutorRatings;
 }
--- app/src/main/java/com/example/sfarmani/ucsctutor/TutorClass.java
+++ app/src/main/java/com/example/sfarmani/ucsctutor/TutorClass.java
@@ -1,7 +1,36 @@
 package com.example.sfarmani.ucsctutor;
 
+import android.media.Image;
+
+import java.util.List;
+
 /**
  * Created by Brad Cardello on 10/14/2015.
  */
 public class TutorClass {
+    String tutorUserName;
+    String tutorDesc;
+    Image tutorProfPic;
+    List<Double> ratings;
+    double ovrRating;
+
+    // Not sure how much actually needs to be done in this function
+    public void editDescription(String newDesc){
+        tutorDesc = newDesc;
+    }
+
+    // Returns average rating of Tutor
+    public double avgRating(List<Double> ratingList) {
+        double sum = 0;
+        for (int i = 0; i < ratings.size(); i++)
+            sum += ratingList.get(i);
+        return sum/ratings.size();
+    }
+
+    // Adds a new student-submitted rating to the ratings list
+    public void addRating(double newRating){
+        this.ratings.add(newRating);
+    }
+
+
 }
