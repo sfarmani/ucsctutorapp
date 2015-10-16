65433b28892f1b1e3ae9959e48c37abb51c639f9
GeorgeAzpeitia <GeorgeAlejandroA@Hotmail.com>
2015-10-16 10:40:05 -0700

    Update TutorClass.java
    I fiddled with the tutor class to try and make things more efficient, modified addRating function and avg function, I can't compile it at school so I'm not 100% if it compiles correctly.

--- app/src/main/java/com/example/sfarmani/ucsctutor/TutorClass.java
+++ app/src/main/java/com/example/sfarmani/ucsctutor/TutorClass.java
@@ -12,6 +12,7 @@ public class TutorClass {
     String tutorDesc;
     Image tutorProfPic;
     List<Double> ratings;
+    List<String> ratingsContent;
     double ovrRating;
 
     // Not sure how much actually needs to be done in this function
@@ -24,12 +25,20 @@ public class TutorClass {
         double sum = 0;
         for (int i = 0; i < ratings.size(); i++)
             sum += ratingList.get(i);
-        return sum/ratings.size();
+        this.ovrRating = sum/ratings.size();
+        return ovrRating;
     }
-
+    
     // Adds a new student-submitted rating to the ratings list
-    public void addRating(double newRating){
+    // We can calculate the running average when we insert the rating, although we
+    // might want to track more things than just a single number for rating and we might want
+    // to create a pair object to hold the rating and the content so we don't have to worry about
+    // and desync issues between the lists.
+    public void addRating(double newRating, String newContent){
         this.ratings.add(newRating);
+        this.ratingsContent.add(newContent);
+        
+        this.ovrRating = this.ovrRating + ((newRating - this.ovrRating)/this.ratings.size());
     }
 
 
